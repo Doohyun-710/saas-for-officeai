458a4b2d69f9d5277d8ce63b881c4b038cf93859
Doohyun-710 <dh1004222@naver.com>
2025-08-26 02:42:44 +0000

    커밋

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('OfficeAI 백엔드 서버입니다!');
+});
+
+app.listen(PORT, () => {
+  console.log(`서버가 http://localhost:${PORT} 에서 실행 중`);
+});
